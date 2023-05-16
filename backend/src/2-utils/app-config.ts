01d4eee0f51bf4a32e445db37530a9fd05b54d3e
Michal Shenkar <0583294585m@gmail.com>
2023-05-16 10:00:15 +0300

    init project

---
+++ backend/src/2-utils/app-config.ts
@@ -0,0 +1,11 @@
+class AppConfig {
+    public host = "localhost"
+    public user = "MyTravelAdmin"
+    public password = "aroundtheworld"
+    public database = "mytravel"
+
+    public port = 3001
+}
+
+const appConfig = new AppConfig()
+export default appConfig
