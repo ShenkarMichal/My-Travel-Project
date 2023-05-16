aba856386eee14f6e5f636b47777d8b19b2af256
Michal Shenkar <0583294585m@gmail.com>
2023-05-16 10:22:47 +0300

    backend - add sanitize middleware

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,10 +2,12 @@ import express from 'express'
 import routeNotFound from './3-middlewares/route-not-found'
 import catchAll from './3-middlewares/catch-all'
 import appConfig from './2-utils/app-config'
+import sanitize from './3-middlewares/sanitize'
 
 const server = express()
 
 server.use(express.json())
+server.use(sanitize)
 
 server.use("*", routeNotFound)
 server.use(catchAll)
