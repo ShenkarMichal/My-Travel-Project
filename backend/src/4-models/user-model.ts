feaf94bcb85c6a0ce0f2d74e520451422a0f7804
Michal Shenkar <0583294585m@gmail.com>
2023-05-16 22:18:17 +0300

    backend - add validationSchema to user-model

--- backend/src/4-models/user-model.ts
+++ backend/src/4-models/user-model.ts
@@ -1,4 +1,5 @@
 import RoleModel from "./role-model"
+import Joi from 'joi'
 
 class UserModel {
     public userID: number
@@ -19,6 +20,22 @@ class UserModel {
         this.role = user.role
     }
 
+    //Validation
+    public static ValidationSchema = Joi.object({
+        userID: Joi.number().optional().integer().positive(),
+        firstName: Joi.string().required().min(3).max(15),
+        lastName: Joi.string().required().min(3).max(15),
+        username: Joi.string().required().min(3).max(15),
+        //The password must contain at least one lowercase letter, at least one uppercase letter, and at least one number:
+        password: Joi.string().required().min(8).regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).*$/), 
+        email: Joi.string().required().email(),
+        role: Joi.string().forbidden()
+    })
+
+    public validate(): string {
+        const resoult = UserModel.ValidationSchema.validate(this)
+        return resoult.error?.message
+    }
 }
 
 export default UserModel
