3ea37af8e14d1aa878086052fa8dd349725d1914
Michal Shenkar <0583294585m@gmail.com>
2023-05-17 12:49:17 +0300

    backend - add login controller

--- backend/src/6-controllers/auth-controller.ts
+++ backend/src/6-controllers/auth-controller.ts
@@ -1,6 +1,7 @@
 import express, { NextFunction, Request, Response } from 'express'
 import UserModel from '../4-models/user-model'
 import authLogic from '../5-logics/auth-logic'
+import CredentialModel from '../4-models/credential-model'
 
 const router = express.Router()
 
@@ -13,6 +14,21 @@ router.post("/register", async (request: Request, response: Response, next: Next
     
         //Response the token
         response.status(201).json(token)        
+    }
+    catch (err: any) {
+        next(err)        
+    }
+})
+
+//Login
+router.post("/login", async (request: Request, response: Response, next: NextFunction)=>{
+    try {
+        //Create credential model from the body
+        const credential = new CredentialModel(request.body)
+        const token = await authLogic.login(credential)
+    
+        //Response the token
+        response.json(token)        
     }
     catch (err: any) {
         next(err)        
