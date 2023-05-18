36a14027757bbfbbd3eead60d0c75a8fa6832a9c
Michal Shenkar <0583294585m@gmail.com>
2023-05-18 13:42:04 +0300

    frontend - add auth-state of redux

---
+++ frontend/src/3-Redux/AuthState.ts
@@ -0,0 +1,49 @@
+import jwtDecode from "jwt-decode";
+import UserModel from "../4-Models/UserModel";
+import { createStore } from "redux";
+
+//1 - Global-State:
+export class AuthState {
+    public user: UserModel = null
+    public token: string = null
+}
+
+//2 - Action-Type:
+export enum AuthActionType {
+    Login,
+    Register,
+    Logout
+}
+
+//3 - Action:
+export interface AuthAction {
+    type: AuthActionType,
+    payload: string
+}
+
+//4 - Reducer:
+export function authReduser(currentState = new AuthState(), action: AuthAction): AuthState {
+    const newState = {...currentState}
+
+    switch (action.type) {
+        case AuthActionType.Register:
+        case AuthActionType.Login:
+            newState.token = action.payload
+            //Rscue the user from the token string:
+            const container: {user: UserModel} = jwtDecode(newState.token)
+            newState.user = container.user
+            //Save the token for refresh-page case:
+            sessionStorage.setItem("token", newState.token)
+            break
+        case AuthActionType.Logout:
+            newState.token = null
+            newState.user = null
+            sessionStorage.removeItem("token")
+            break
+    }
+
+    return newState
+}
+
+//5 - Store
+export const authStore = createStore(authReduser)
