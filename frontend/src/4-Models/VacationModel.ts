26ac6374bdb096450a76e0def9882ea1652ea6be
Michal Shenkar <0583294585m@gmail.com>
2023-05-21 15:10:52 +0300

    frontend - add vacationModel

---
+++ frontend/src/4-Models/VacationModel.ts
@@ -0,0 +1,14 @@
+
+class VacationModel {
+    public vacationID: number
+    public destination: string
+    public description: string
+    public startDate: string
+    public endDate: string
+    public daysDiff: number
+    public price: number
+    public imageName: string
+    public image: UploadedFile
+}
+
+export default VacationModel
---
+++ frontend/src/Components/VacationsArea/VacationCard/VacationCard.tsx
@@ -0,0 +1,15 @@
+import "./VacationCard.css";
+
+interface VacationCardProps {
+    vacation: Vacatio
+}
+
+function VacationCard(props: VacationCardProps): JSX.Element {
+    return (
+        <div className="VacationCard">
+			
+        </div>
+    );
+}
+
+export default VacationCard;
