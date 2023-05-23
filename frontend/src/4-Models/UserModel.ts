269ea95d903d95bf3b72dd84bf49c6d014f2bb8a
Michal Shenkar <0583294585m@gmail.com>
2023-05-23 14:10:35 +0300

    frontend - add validation objects in user-model

--- frontend/src/4-Models/UserModel.ts
+++ frontend/src/4-Models/UserModel.ts
@@ -8,6 +8,27 @@ class UserModel {
     public password: string
     public email: string
     public role: RoleModel
+
+    public static firstNameValidatoin = {
+        required: {value: true, message: "First-Name is missing"},
+        min: {value: 3, message: "First-Name is too short"},
+        max: {value: 15, message: "First-Name is too long"}
+    }
+    public static lastNameValidatoin = {
+        required: {value: true, message: "Last-Name is missing"},
+        min: {value: 3, message: "Last-Name is too short"},
+        max: {value: 15, message: "Last-Name is too long"}
+    }
+    public static usernameValidatoin = {
+        required: {value: true, message: "username is missing"},
+        min: {value: 3, message: "username is too short"},
+        max: {value: 15, message: "username is too long"}
+    }
+    public static passwordValidatoin = {
+        required: {value: true, message: "Password is missing"},
+        min: {value: 8, message: "Password must contain at least 8 characters"},
+        pattern: {value: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).*$/, message: "Password must contain at least one uppercase letter, one lowercase letter, and one number"}        
+    }
 }
 
 export default UserModel
