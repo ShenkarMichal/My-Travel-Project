1ca82698a11990887085e4334490b35fd38976e8
Michal Shenkar <0583294585m@gmail.com>
2023-05-25 01:32:32 +0300

    frontend - add continent-sentences file

---
+++ frontend/src/2-Utils/ContinentsSentences.ts
@@ -0,0 +1,20 @@
+
+enum ContinentsSentences {
+    Asia = "There should not be a contradiction between development and tradition. Countries like Japan and South Korea have grown their economies greatly while maintaining unique cultures",
+    AsiaSrc = "Barack Obama",
+
+    Europe = "A war between Europeans is a civil war.",
+    EuropeSrc = "Victor Hugo",
+
+    Antarctica = "The common heritage of mankind.",
+    AntarcticaSrc = "Antarctic Treaty",
+
+    Australia = "The soil here is so generous; Tickle her with a hoe and she'll laugh with a crop.",
+    AustraliaSrc = "Douglas William Jerrold",
+
+    NorthAmerica = "Our country can be compared to a new home. It lacks many things, but it has the most important thing of all - freedom.",
+    NorthAmericaSrc = "James Monroe",
+
+    SouthAmerica = "I want to live like a cultured South American and not kill the Indians like the Europeans",
+    SouthAmericaSrc = "Simon Bolivar"
+}
