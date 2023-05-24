6d4ccb3ffa0b9fedf3cbf6b0fb03035c31eca134
Michal Shenkar <0583294585m@gmail.com>
2023-05-25 01:08:57 +0300

    frontend - add pagination in the continent-area

--- frontend/src/Components/VacationsArea/ContinentArea/ContinentArea.tsx
+++ frontend/src/Components/VacationsArea/ContinentArea/ContinentArea.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
 import VacationModel from "../../../4-Models/VacationModel";
 import "./ContinentArea.css";
 import vacationService from "../../../5-Service/VacationsService";
@@ -10,12 +10,20 @@ import FormControl from '@mui/material/FormControl';
 import Select, { SelectChangeEvent } from '@mui/material/Select';
 import appConfig from "../../../2-Utils/Config";
 import Earth from "../../../1-Assets/Images/BackGrounds/Earth.jpg"
+import VacationCard from "../VacationCard/VacationCard";
+import Pagination from '@mui/material/Pagination';
 
 function ContinentArea(): JSX.Element {
 
     const [vacations, setVacations] = useState<VacationModel[]>([])
 
     const [continents, setContinents] = useState<ContinentModel[]>([])
+    //Set Pagination:
+    const [pageNumber, setPageNumber] = useState<number>(0)
+    const [currentPage, setCurrentPage] = useState<number>(1)
+
+
+    const vacationPerPage = 3
 
     //Get all continents:
     useEffect(()=>{
@@ -33,16 +41,24 @@ function ContinentArea(): JSX.Element {
             if(continentID === 0){
                 setVacations([])
                 setContinentName("")
+                setPageNumber(0)
             }
             else {
                 const vacationsByContinent = await vacationService.getVacationsByContinent(continentID)
                 setVacations(vacationsByContinent)
                 setContinentName(vacationsByContinent[0].continentName)
+                //Set pagination:
+                setPageNumber(Math.ceil(vacationsByContinent.length/vacationPerPage))
             }
         }
         catch(err:any) {
             alert(err.response.data)
         }        
+    }
+
+    function HandleChangePage(event:ChangeEvent<unknown>, newPage: number) {
+
+        setCurrentPage(newPage)
     }
 
     return (
@@ -51,13 +67,15 @@ function ContinentArea(): JSX.Element {
 			<div 
                 className="background" 
                 style={{backgroundImage:`url(${appConfig.continentsImageURL + continentName}), url(${Earth})`}}></div>
-                <div className="content" style={{backgroundImage:`url(${appConfig.continentsImageURL + continentName}), url(${Earth})`}}>
+            <div className="content" style={{backgroundImage:`url(${appConfig.continentsImageURL + continentName}), url(${Earth})`}}>
                 <FormControl sx={{ m: 1, minWidth: 120 }}>
                     <InputLabel id="demo-simple-select-helper-label">Continent</InputLabel>
                     <Select
                         labelId="demo-simple-select-helper-label"
                         id="demo-simple-select-helper"
-                        label="Age"
+                        label="Continent"
+                        className="Select"
+                        variant="outlined"
                         onChange={getVacations}
                         >
                         <MenuItem value="0">
@@ -66,10 +84,24 @@ function ContinentArea(): JSX.Element {
                         {continents.map(c => <MenuItem value={c.continentID} key={c.continentID}>{c.continentName}</MenuItem>)}
                     </Select>
                     <FormHelperText>Select the continent you want to travel to</FormHelperText>
-            </FormControl>
-   
-                    <h3>{continentName}</h3>
-			    vacations list
+                </FormControl>
+                <div className="AllData">
+                    <div className="Heading">
+                        <h3>{continentName}</h3>
+                    </div>
+                    <div className="Vacations">
+
+                        {vacations && vacations.slice((currentPage-1)* vacationPerPage, currentPage * vacationPerPage).
+                                                map(v => 
+                                                    <div className="card">
+                                                        <span>{v.duration} days in {v.destination}</span><br/>
+                                                        <VacationCard key={v.vacationID} vacation={v}/>
+                                                        <span>Just {v.price}$</span>
+                                                    </div>)}
+                    </div>
+                </div>
+                <Pagination className="Pagination" count={pageNumber} page={currentPage} onChange={HandleChangePage} color="primary"/>
+
             </div>
         </div>
     );
