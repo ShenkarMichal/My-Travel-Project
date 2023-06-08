431d9efe9ff47e02fa32da804125a745aaf2c99e
Michal Shenkar <0583294585m@gmail.com>
2023-06-08 14:58:19 +0300

    frontend - add the vacation report page + style

--- frontend/src/Components/VacationsArea/VacationsReport/VacationsReport.tsx
+++ frontend/src/Components/VacationsArea/VacationsReport/VacationsReport.tsx
@@ -1,7 +1,11 @@
 import { useEffect, useState } from "react";
 import "./VacationsReport.css";
-import { VictoryBar, VictoryLabel, VictoryPie, VictorySharedEvents } from 'victory';
 import followersService from "../../../5-Service/FollowersService";
+// import CanvasJSReact from 'canvasjs-react-charts';
+
+const Canvas = require("canvasjs-react-charts") 
+var CanvasJSChart = Canvas.CanvasJSChart;
+
 
 function VacationsReport(): JSX.Element {
 
@@ -12,66 +16,60 @@ function VacationsReport(): JSX.Element {
             .catch(err => console.log(err))        
     },[])
 
-    const dataGragh: any[] = data.map(d => { return {x: d.destination, y: d.followers_count }})
-    console.log(dataGragh)
-    const labels = data.map(d => d.destination)
+    const dataGragh: any[] = data.map(d => { return {y: d.followers_count, label: d.destination }})
+
+    //Set the Pie settings:
+    const pieOptions = {
+        // exportEnabled: true,
+        animationEnabled: true,
+        
+        title: {
+            text: ""
+        },
+        data: [{
+            type: "pie",
+            startAngle: 75,
+            toolTipContent: "<b>{label}</b>: {y} followers",
+            showInLegend: "true",
+            legendText: "{label}",
+            indexLabelFontSize: 16,
+            indexLabel: "{label} - {y} followers",
+            dataPoints:dataGragh
+        }]
+    }
+
+    const barOptions = {
+        title: {
+            text: ""
+        },
+        animationEnabled: true,
+        data: [
+        {
+            // Change type to "doughnut", "line", "splineArea", etc.
+            type: "column",
+            dataPoints: dataGragh
+        }
+        ]
+    }
+
     return (
         <div className="VacationsReport">
-            <>
-            {dataGragh &&
-                <svg viewBox="0 0 650 350">
-                    <VictorySharedEvents
-                        events={[{
-                        childName: ["pie", "bar"],
-                        target: "data",
-                        eventHandlers: {
-                            onMouseOver: () => {
-                            return [{
-                                childName: ["pie", "bar"],
-                                mutation: (props) => {
-                                return {
-                                    style: Object.assign({}, props.style, {fill: "tomato"})
-                                };
-                                }
-                            }];
-                            },
-                            onMouseOut: () => {
-                            return [{
-                                childName: ["pie", "bar"],
-                                mutation: () => {
-                                return null;
-                                }
-                            }];
-                            }
-                        }
-                        }]}
-                    >
-                        <g transform={"tran slate(150, 50)"}>                    
-                            <VictoryBar name="bar"
-                                width={300}
-                                data={[dataGragh][0]}
-
-                                standalone={false}
-                                style={{
-                                labels: {fontSize: 20}
-                                }}   
-                                labels={labels}                        
-                                labelComponent={<VictoryLabel text={labels}/>}
-                            />
-                        </g>
-                        <g transform={"translate(0, -75)"}>
-                            <VictoryPie name="pie"
-                                width={250}
-                                standalone={false}
-                                style={{ labels: {fontSize: 25, padding: 10}}}
-                                labels={labels}
-                                data={dataGragh}
-                            />
-                        </g>
-                    </VictorySharedEvents>
-                </svg>
-            }</>
-
+            <div className="Background"></div>
+            <div className="Content">
+                <div className="Heading">
+                    <h3>Vacations Followers Report</h3>
+                </div>
+                <div className="AllData">
+                    <div className="Charts">            
+                        <div className="canvas">
+                            <CanvasJSChart options = {pieOptions} />
+                        </div>	
+                        <div className="canvas">
+                            <CanvasJSChart options = {barOptions} />
+                        </div>	
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
