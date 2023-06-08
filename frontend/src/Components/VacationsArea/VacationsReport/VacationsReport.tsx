dfb6f9cfbd111723f1bbef3e70fc6fa1dd916d23
Michal Shenkar <0583294585m@gmail.com>
2023-06-08 15:12:11 +0300

    frontend - add checkbox for change the display charts

--- frontend/src/Components/VacationsArea/VacationsReport/VacationsReport.tsx
+++ frontend/src/Components/VacationsArea/VacationsReport/VacationsReport.tsx
@@ -1,7 +1,11 @@
 import { useEffect, useState } from "react";
 import "./VacationsReport.css";
 import followersService from "../../../5-Service/FollowersService";
-// import CanvasJSReact from 'canvasjs-react-charts';
+import { Checkbox } from "@mui/material";
+import ViewColumnRoundedIcon from '@mui/icons-material/ViewColumnRounded';
+import ViewWeekOutlinedIcon from '@mui/icons-material/ViewWeekOutlined';
+import PieChartOutlineOutlinedIcon from '@mui/icons-material/PieChartOutlineOutlined';
+import PieChartRoundedIcon from '@mui/icons-material/PieChartRounded';
 
 const Canvas = require("canvasjs-react-charts") 
 var CanvasJSChart = Canvas.CanvasJSChart;
@@ -15,8 +19,26 @@ function VacationsReport(): JSX.Element {
             .then(d => setData(d))
             .catch(err => console.log(err))        
     },[])
-
     const dataGragh: any[] = data.map(d => { return {y: d.followers_count, label: d.destination }})
+
+    const [pie, setPie] = useState<boolean>(true)
+    const [column, setColumn] = useState<boolean>(false)
+
+    async function handlePie(event: React.ChangeEvent<HTMLInputElement>) {
+        setPie(event.target.checked)
+        
+        if(!pie){
+            setColumn(false)
+        }
+    }
+
+    async function handleColumn(event: React.ChangeEvent<HTMLInputElement>) {
+        setColumn(event.target.checked)
+        
+        if(!column){
+            setPie(false)
+        }
+    }
 
     //Set the Pie settings:
     const pieOptions = {
@@ -53,6 +75,7 @@ function VacationsReport(): JSX.Element {
     }
 
     return (
+        
         <div className="VacationsReport">
             <div className="Background"></div>
             <div className="Content">
@@ -60,13 +83,32 @@ function VacationsReport(): JSX.Element {
                     <h3>Vacations Followers Report</h3>
                 </div>
                 <div className="AllData">
-                    <div className="Charts">            
-                        <div className="canvas">
-                            <CanvasJSChart options = {pieOptions} />
-                        </div>	
-                        <div className="canvas">
-                            <CanvasJSChart options = {barOptions} />
-                        </div>	
+                    <div className="Charts">  
+                        <Checkbox 
+                            checked = {pie}
+                            onChange={handlePie}
+                            title="Pie display" 
+                            icon={<PieChartOutlineOutlinedIcon 
+                            sx={{fontSize: 40}}/>} 
+                            checkedIcon={<PieChartRoundedIcon color="error" sx={{fontSize: 40}}/>} 
+                        />
+                        <Checkbox
+                            checked = {column}
+                            onChange={handleColumn}
+                            title="Column display"
+                            icon={<ViewWeekOutlinedIcon sx={{fontSize: 40}} />}
+                            checkedIcon={<ViewColumnRoundedIcon color="error" sx={{fontSize: 40}} />}                    
+                        /> 
+                        {pie &&         
+                            <div className="canvas">
+                                <CanvasJSChart options = {pieOptions} />
+                            </div>
+                        }
+                        {column &&	
+                            <div className="canvas">
+                                <CanvasJSChart options = {barOptions} />
+                            </div>	
+                        }
                     </div>
                 </div>
             </div>
